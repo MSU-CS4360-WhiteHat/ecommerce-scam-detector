34c9989d9392c2c1b4b50d2324b5775c5a624176
thrandale <64854392+thrandale@users.noreply.github.com>
2023-02-08 16:58:58 -0700

    added prettier formatter (#30)

--- background.js
+++ background.js
@@ -1,82 +1,84 @@
 // Arbitrary numbers, we should adjust them as needed.
 const Risk = {
-    HIGH: 3,
-    MED: 2,
-    LOW: 1,
-    UNKNOWN: 0
-}
+  HIGH: 3,
+  MED: 2,
+  LOW: 1,
+  UNKNOWN: 0,
+};
 
 // https://stackoverflow.com/questions/34818020/javascript-regex-url-extract-domain-only
 function domain_from_url(url) {
-    var result
-    var match
-    if (match = url.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n\?\=]+)/im)) {
-        result = match[1]
-        if (match = result.match(/^[^\.]+\.(.+\..+)$/)) {
-            result = match[1]
-        }
+  var result;
+  var match;
+  if (
+    (match = url.match(
+      /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n\?\=]+)/im
+    ))
+  ) {
+    result = match[1];
+    if ((match = result.match(/^[^\.]+\.(.+\..+)$/))) {
+      result = match[1];
     }
-    return result
+  }
+  return result;
 }
-
 
 // Called when the user clicks on the extension's icon.
 browser.browserAction.onClicked.addListener(function (tab) {
-    toggleIcon();
+  toggleIcon();
 });
 
 // Called before the Navigation occurs.
-browser.webNavigation.onBeforeNavigate.addListener(function(details) {
+browser.webNavigation.onBeforeNavigate.addListener(function (details) {
+  // Consider moving this to onCompleted if onBefore is never used.
+  const domain = domain_from_url(details.url);
+  const riskStatus = localStorage.getItem(domain);
 
-    // Consider moving this to onCompleted if onBefore is never used.
-    const domain = domain_from_url(details.url);
-    const riskStatus = localStorage.getItem(domain);
+  if (riskStatus == Risk.HIGH) {
+    // Consider stopping page load and asking the user if they wish to continue
+    // if we deep scan here and find some oddities.
+    console.log("Risk is HIGH");
+  } else if (!riskStatus) {
+    // Consider a please wait while we check if the site is safe?
+    // Deep scan here.
+    //  If this causes a huge latency before page load, we can move it to onCompleted.
 
-    if(riskStatus == Risk.HIGH){
-        // Consider stopping page load and asking the user if they wish to continue 
-        // if we deep scan here and find some oddities. 
-        console.log("Risk is HIGH")
-    }else if(!riskStatus){
-        // Consider a please wait while we check if the site is safe?
-        // Deep scan here.
-        //  If this causes a huge latency before page load, we can move it to onCompleted.
-        
-        console.log("Site "+domain+" has not been scanned, scanning now.")
-        // TODO replace with an method call to the application service.
-        // e.g. const riskResults = someApiFunctionCall() ?? Risk.UNKNOWN
-        const riskResults = Risk.UNKNOWN 
-        localStorage.setItem(domain, riskResults);
-    }else{
-        // TODO Debugging else, remove.
-        console.log("Site "+domain+" has already been scanned--do nothing")
-    }
+    console.log("Site " + domain + " has not been scanned, scanning now.");
+    // TODO replace with an method call to the application service.
+    // e.g. const riskResults = someApiFunctionCall() ?? Risk.UNKNOWN
+    const riskResults = Risk.UNKNOWN;
+    localStorage.setItem(domain, riskResults);
+  } else {
+    // TODO Debugging else, remove.
+    console.log("Site " + domain + " has already been scanned--do nothing");
+  }
 });
 
 // Called when the user navigates to a new page.
 browser.webNavigation.onCompleted.addListener(function (details) {
-    console.log("Navigated to: " + details.url);
+  console.log("Navigated to: " + details.url);
 });
 
 // Temp Code for testing the toggleIcon function
 let color = "red";
 function toggleIcon() {
-    if (color === "red") {
-        color = "yellow";
-    } else if (color === "yellow") {
-        color = "green";
-    } else {
-        color = "red";
-    }
+  if (color === "red") {
+    color = "yellow";
+  } else if (color === "yellow") {
+    color = "green";
+  } else {
+    color = "red";
+  }
 
-    setIcon(color);
+  setIcon(color);
 }
 
 // Sets the extension's icon to the specified color.
 // Available colors: "red", "yellow", "green"
 function setIcon(color) {
-    try {
-        browser.browserAction.setIcon({ path: "/icons/" + color + "-circle.png" });
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    browser.browserAction.setIcon({ path: "/icons/" + color + "-circle.png" });
+  } catch (e) {
+    console.log(e);
+  }
 }
