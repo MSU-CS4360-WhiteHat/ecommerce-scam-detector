4e82aa72dd85a4c354ed178bdcb2d0da78866873
Theo Crandall <thrandale@gmail.com>
2023-02-14 17:12:04 -0700

    added api requesting

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ const Risk = {
 };
 
 function domain_from_url(url) {
-  return url.split('/')[2];
+  return url.split("/")[2];
 }
 
 // Called when the user clicks on the extension's icon.
@@ -17,16 +17,6 @@ browser.browserAction.onClicked.addListener(function (tab) {
 
 // Called before the Navigation occurs.
 browser.webNavigation.onBeforeNavigate.addListener(function (details) {
-  // Consider moving this to onCompleted if onBefore is never used.
-  const domain = domain_from_url(details.url);
-
-  if(!domain){
-    // No domain found, so just return.
-    return 1;
-  }
-
-  const riskStatus = localStorage.getItem(domain);
-
   if (riskStatus == Risk.HIGH) {
     // Consider stopping page load and asking the user if they wish to continue
     // if we deep scan here and find some oddities.
@@ -35,7 +25,6 @@ browser.webNavigation.onBeforeNavigate.addListener(function (details) {
     // Consider a please wait while we check if the site is safe?
     // Deep scan here.
     //  If this causes a huge latency before page load, we can move it to onCompleted.
-
     console.log("Site " + domain + " has not been scanned, scanning now.");
     // TODO replace with an method call to the application service.
     // e.g. const riskResults = someApiFunctionCall() ?? Risk.UNKNOWN
@@ -50,6 +39,26 @@ browser.webNavigation.onBeforeNavigate.addListener(function (details) {
 // Called when the user navigates to a new page.
 browser.webNavigation.onCompleted.addListener(function (details) {
   console.log("Navigated to: " + details.url);
+  const domain = domain_from_url(details.url);
+
+  if (!domain) {
+    // No domain found, so just return.
+    console.log("No domain found, so just return.");
+    return 1;
+  }
+  console.log("Getting data from local storage for: " + domain);
+
+  const data = localStorage.getItem(domain);
+
+  if (data) {
+    console.log("Site " + domain + " has already been scanned");
+    console.log("Data for " + domain + " is: " + data);
+    return 1;
+  } else {
+    makeWOTRequest(domain, function (json) {
+      localStorage.setItem(domain, json);
+    });
+  }
 });
 
 // Temp Code for testing the toggleIcon function
@@ -74,4 +83,25 @@ function setIcon(color) {
   } catch (e) {
     console.log(e);
   }
+}
+
+function makeWOTRequest(url, callback) {
+  let WOTUrl = "https://scorecard.api.mywot.com/v3/targets?t=";
+  let requestUrl = WOTUrl + url;
+
+  headers = {
+    "x-user-id": "8866427",
+    "x-api-key": "f2b8ef8f223b9943ba9512bc516d375c05a63613",
+  };
+
+  console.log("Making API request to: " + requestUrl);
+
+  fetch(requestUrl, {
+    method: "GET",
+    headers: headers,
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      callback(JSON.stringify(json));
+    });
 }
