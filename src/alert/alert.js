8dba447669fcb50504f705db92d76c4806e1cf07
Theo Crandall <thrandale@gmail.com>
2023-03-09 23:36:12 -0700

    Refactored and made the alert and buttons work

--- src/alert/alert.js
+++ src/alert/alert.js
@@ -1,144 +1,42 @@
 const app = window?.chrome ?? browser;
 
-let loadAgain = true;
-let loaded = false;
+const title = document.getElementById("title");
+const subTitle = document.getElementById("sub-title");
+const rankNumber = document.getElementById("ranking");
+const message = document.getElementById("message");
+const reasons = document.getElementById("reasons");
 
-function loadNotSecure(values) {
-  const title = values?.title;
-  const subTitle = values?.subTitle;
-  const rankNumber = values?.rankNumber;
-  const message = values?.message;
-  const reasons = values?.reasons;
+const stay = document.getElementById("stay");
+const leave = document.getElementById("leave");
 
-  document.body.classList.add("stop-scrolling");
-  window.scrollTo(0, 0);
-  document.body.parentElement.classList.add("stop-scrolling");
-
-  let backdrop = document.createElement("div");
-  let container = document.createElement("div");
-  let innerContent = document.createElement("div");
-  let hOneTitle = document.createElement("h1");
-  let hThreeSubTitle = document.createElement("h3");
-  let hTwoRank = document.createElement("h2");
-  let pMessage = document.createElement("p");
-  let orderedList = document.createElement("ol");
-  let buttons = document.createElement("div");
-  let buttonContainer = document.createElement("div");
-  let stay = document.createElement("button");
-  let leave = document.createElement("button");
-
-  backdrop.className += "container backdrop";
-  container.className += "container top-container";
-  innerContent.className += "innercontent";
-  hThreeSubTitle.className += "shadoweffect";
-  buttons.className += "buttons";
-  buttonContainer.className += "buttons-container-control";
-  stay.className += "btn btn-2s btn-2as";
-  leave.className += "btn btn-2 btn-2a";
-
-  hOneTitle.innerHTML = title;
-  hThreeSubTitle.innerHTML = subTitle;
-  hTwoRank.innerHTML = rankNumber;
-  pMessage.innerHTML = message;
-
-  reasons.forEach((element) => {
-    let listItem = document.createElement("li");
-    listItem.innerHTML = element;
-    orderedList.appendChild(listItem);
+// Send messages to background script to close the alert
+stay.addEventListener("click", () => {
+  app.runtime.sendMessage({
+    type: "close_alert",
+    shouldStay: true,
   });
-
-  stay.innerHTML = "Stay";
-  leave.innerHTML = "Leave";
-
-  backdrop.id = "main-backdrop";
-  container.id = "main-container";
-  stay.id = stay.innerHTML.toLowerCase();
-  leave.id = leave.innerHTML.toLowerCase();
-
-  document.body.appendChild(backdrop);
-  document.body.appendChild(container);
-  container.appendChild(innerContent);
-  innerContent.appendChild(hOneTitle);
-  innerContent.appendChild(hThreeSubTitle);
-  innerContent.appendChild(hTwoRank);
-  innerContent.appendChild(pMessage);
-  innerContent.appendChild(orderedList);
-  innerContent.appendChild(buttons);
-  buttons.appendChild(buttonContainer);
-  buttonContainer.appendChild(stay);
-  buttonContainer.appendChild(leave);
-}
-
-// https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
-function waitForElm(selector) {
-  return new Promise((resolve) => {
-    if (document.querySelector(selector)) {
-      return resolve(document.querySelector(selector));
-    }
-
-    const observer = new MutationObserver((mutations) => {
-      if (document.querySelector(selector)) {
-        resolve(document.querySelector(selector));
-        observer.disconnect();
-      }
-    });
-
-    observer.observe(document.body, {
-      childList: true,
-      subtree: true,
-    });
-  });
-}
-
-const closePopup = (e) => {
-  console.log(e?.target?.id);
-  document.body.classList.remove("stop-scrolling");
-  document.body.parentElement.classList.remove("stop-scrolling");
-  document.getElementById("main-container").remove();
-  document.getElementById("main-backdrop").remove();
-};
-
-const closeTab = (e) => {
-  console.log(e?.target?.id);
-  browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    // send a data request to the background script
-    browser.runtime.sendMessage(
-      { type: "close_current_tab", url: tabs[0].url },
-      (response) => {
-        updateInfo(response.data);
-      }
-    );
-  });
-};
-
-app.runtime.onMessage.addListener((request, sender) => {
-  console.log("Message Received from Background to alert");
-  console.log(request);
-  if (loadAgain && !loaded) {
-    loadNotSecure(request);
-  }
-  waitForElm("#stay").then((elm) => {
-    console.log("Element is ready");
-    console.log(elm.textContent);
-    let stay = document.getElementById("stay");
-    stay.addEventListener("click", closePopup, false);
-    loadAgain = false;
-  });
-  waitForElm("#leave").then((elm) => {
-    console.log("Element is ready");
-    console.log(elm.textContent);
-    let stay = document.getElementById("leave");
-    stay.addEventListener("click", closeTab, false);
-  });
-  return Promise.resolve({ response: "Loaded the popup" });
 });
 
-browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  // send a data request to the background script
-  browser.runtime.sendMessage(
-    { type: "close_current_tab", url: tabs[0]?.url },
-    (response) => {
-      updateInfo(response.data);
-    }
-  );
+leave.addEventListener("click", () => {
+  app.runtime.sendMessage({
+    type: "close_alert",
+    shouldStay: false,
+  });
+});
+
+// listen for messages from the background script to open the alert
+app.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === "open_alert") {
+    // set the data in the alert
+    title.innerText = request.payload.title;
+    subTitle.innerText = request.payload.subTitle;
+    rankNumber.innerText = request.payload.rankNumber;
+    message.innerText = request.payload.message;
+    reasons.innerHTML = "";
+    request.payload.reasons.forEach((reason) => {
+      const li = document.createElement("li");
+      li.innerText = reason;
+      reasons.appendChild(li);
+    });
+  }
 });
---
+++ src/content/content.js
@@ -0,0 +1,45 @@
+const app = window?.chrome ?? browser;
+
+// keeps track of whether the alert is open
+// otherwise it will infinitely open the alert
+let isAlertOpen = false;
+
+// the iframe that displays the alert
+let iframe;
+
+// opens the alert
+function openAlert() {
+  if (isAlertOpen) {
+    return;
+  }
+  // create an iframe to display the alert
+  // the iframe sits as the first child of the body but fills the entire page
+  iframe = document.createElement("iframe");
+  iframe.src = app.runtime.getURL("src/alert/alert.html");
+  iframe.id = "scam-detector-alert-iframe";
+
+  // insert the iframe above the header
+  document.body.insertBefore(iframe, document.body.firstChild);
+
+  // disable scrolling on the page
+  document.body.style.overflow = "hidden";
+
+  isAlertOpen = true;
+}
+
+// listen for messages from the background script
+app.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === "open_alert") {
+    openAlert(request.tabId);
+  } else if (request.type === "close_alert") {
+    // close the alert
+    iframe.remove();
+    document.body.style.overflow = "auto";
+    isAlertOpen = false;
+
+    // if the user wants to leave the site, redirect them
+    if (!request.shouldStay) {
+      window.location.href = "https://www.google.com/";
+    }
+  }
+});
