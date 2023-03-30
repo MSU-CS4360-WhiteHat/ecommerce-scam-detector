1b9b9213294e70628ff99a0a12352a198f74855d
Theo Crandall <thrandale@gmail.com>
2023-03-30 14:42:41 -0600

    removed api key

--- src/background/background.js
+++ src/background/background.js
@@ -84,8 +84,8 @@ async function makeWOTRequest(url, callback) {
 
   headers = {
     // NOTE: Add the API key and user ID here.
-    "x-user-id": "8866427",
-    "x-api-key": "f2b8ef8f223b9943ba9512bc516d375c05a63613",
+    "x-user-id": "",
+    "x-api-key": "",
   };
 
   console.log("Making API request to: " + requestUrl);
