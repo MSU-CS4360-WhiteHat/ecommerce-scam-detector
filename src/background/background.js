03f0c5a9c7ad78c8d0fc5d2b77b9d52855a88729
Theo Crandall <thrandale@gmail.com>
2023-03-30 20:24:32 -0600

    rebased off dev

--- src/background/background.js
+++ src/background/background.js
@@ -6,12 +6,20 @@ let hasSSLCert = false;
 
 let weight = 100; // we can rename to score.
 
-const testPayload = {
+const Icons = {
+  SAFE: "safe",
+  WARN: "warning",
+  UNSAFE: "unsafe",
+  DEFAULT: "default",
+};
+
+// TODO see if we can make this a state, similar to react states @see https://www.w3schools.com/react/react_state.asp
+let payloadForUserPrompt = {
   title: "This site seems unsafe!",
   subTitle: "Our ranking",
-  rankNumber: 43,
+  rankNumber: 0,
   message: "Here are the reasons this site could be harmful.",
-  reasons: ["Bad", "Awful", "No Good"],
+  reasons: [],
 };
 
 function domain_from_url(url) {
@@ -22,16 +30,96 @@ function onError(error) {
   console.error(`Error: ${error}`);
 }
 
+/*
+---- When we determine the score and reasons it is unsafe use the following
+  
+payloadForUserPrompt = {
+    title: "This site seems unsafe!",
+    subTitle: "Our ranking",
+    rankNumber: 43,
+    message: "Here are the reasons this site could be harmful.",
+    reasons: ["Bad", "Awful", "No Good"],
+  };
+
+  or 
+
+  payloadForUserPrompt.rankNumber = 43;
+  payloadForUserPrompt.reasons = [...];
+
+---- When we want to trigger the prompt after we determine an score, use the following
+  
+browser.tabs
+  .query({
+    currentWindow: true,
+  })
+  .then(sendMessageToTabs)
+  .catch(onError);
+
+*/
+function sendMessageToTabs(tabs) {
+  for (const tab of tabs) {
+    browser.tabs
+      .sendMessage(tab.id, payloadForUserPrompt)
+      .then((response) => {
+        console.log("Message from the content script:");
+        console.log(response.response);
+      })
+      .catch(onError);
+  }
+}
+
+// Sets the extension's icon to the specified color.
+function setIcon(status = "default") {
+  console.debug("setting status to: ", status);
+  try {
+    browser.browserAction.setIcon({ path: "/icons/" + status + ".svg" });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function makeWOTRequest(url, callback) {
+  const WOTUrl = "https://scorecard.api.mywot.com/v3/targets?t=";
+  const requestUrl = WOTUrl + url;
+
+  headers = {
+    // NOTE: Add the API key and user ID here.
+    "x-user-id": "8866427",
+    "x-api-key": "f2b8ef8f223b9943ba9512bc516d375c05a63613",
+  };
+
+  console.log("Making API request to: " + requestUrl);
+
+  /**
+   * In summary, use await fetch() when you need to perform some operation with the data obtained
+   * from the HTTP response before proceeding with the next line of code, and use fetch() with
+   * then() when you want to perform multiple asynchronous operations in a specific order.
+   */
+  // NOTE:  If we want to switch back to using then, we can.
+  //        This tells it to wait and prevents null errors.
+  const response = await fetch(requestUrl, {
+    method: "GET",
+    headers: headers,
+  });
+  const json = await response.json();
+
+  callback(json);
+}
+
+function getData(url) {
+  const domain = domain_from_url(url);
+  let localStorageData = localStorage.getItem(domain);
+  if (localStorageData) {
+    return JSON.parse(localStorageData);
+  } else {
+    return "No data found";
+  }
+}
+
 // listen for a data request from the popup script
 browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.type == "get_data") {
-    const domain = domain_from_url(request.url);
-    let data = localStorage.getItem(domain);
-    if (data) {
-      sendResponse({ data: data });
-    } else {
-      sendResponse({ data: "No data found" });
-    }
+    sendResponse({ data: getData(request.url) });
   }
 
   if (request.type == "alert_opened") {
@@ -62,96 +150,6 @@ browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
           shouldStay: request.shouldStay,
         });
       });
-  }
-});
-
-// Called when the user navigates to a new page.
-browser.webNavigation.onCompleted.addListener(function (details) {
-  // send a message to the content script to open the alert
-  browser.tabs
-    .query({
-      currentWindow: true,
-      active: true,
-    })
-    .then((tabs) => {
-      console.log("Sending message to open alert");
-      browser.tabs.sendMessage(tabs[0].id, {
-        type: "open_alert",
-        payload: testPayload,
-      });
-    });
-  console.log("Navigated to: " + details.url);
-  const domain = domain_from_url(details.url);
-
-  if (!domain) {
-    // No domain found, so just return.
-    console.log("No domain found, so just return.");
-    return 1;
-  }
-  console.log("Getting data from local storage for: " + domain);
-
-  let data = localStorage.getItem(domain);
-
-  if (data) {
-    console.log("Site " + domain + " has already been scanned");
-    console.log("Data for " + domain + " is: " + data);
-    data = JSON.parse(data);
-  } else {
-    makeWOTRequest(domain, function (json) {
-      localStorage.setItem(domain, json);
-      data = json;
-    });
-  }
-});
-
-// Sets the extension's icon to the specified color.
-// Available colors: "red", "yellow", "green"
-function setIcon(color) {
-  try {
-    browser.browserAction.setIcon({ path: "/icons/" + color + "-circle.png" });
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-async function makeWOTRequest(url, callback) {
-  const WOTUrl = "https://scorecard.api.mywot.com/v3/targets?t=";
-  const requestUrl = WOTUrl + url;
-
-  headers = {
-    // NOTE: Add the API key and user ID here.
-    "x-user-id": "",
-    "x-api-key": "",
-  };
-
-  console.log("Making API request to: " + requestUrl);
-
-  /**
-   * In summary, use await fetch() when you need to perform some operation with the data obtained
-   * from the HTTP response before proceeding with the next line of code, and use fetch() with
-   * then() when you want to perform multiple asynchronous operations in a specific order.
-   */
-  // NOTE:  If we want to switch back to using then, we can.
-  //        This tells it to wait and prevents null errors.
-  const response = await fetch(requestUrl, {
-    method: "GET",
-    headers: headers,
-  });
-  const json = await response.json();
-
-  callback(json);
-}
-
-// listen for a data request from the popup script
-browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.type == "get_data") {
-    const domain = domain_from_url(request.url);
-    let localStorageData = localStorage.getItem(domain);
-    if (localStorageData) {
-      sendResponse({ data: localStorageData });
-    } else {
-      sendResponse({ data: "No data found" });
-    }
   }
 });
 
@@ -197,7 +195,10 @@ browser.webNavigation.onCompleted.addListener(function (details) {
 
       localStorage.setItem(
         domain,
-        json.length > 0 ? JSON.stringify(json[0]) : null
+        JSON.stringify({
+          wot: json.length > 0 ? json[0] : null,
+          score: weight,
+        })
       );
       console.warn(weight);
       // TODO send weight to the popup.
