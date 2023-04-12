a6fead3a13c14b50a4171388c79919df56025d9e
corywilson88 <cwilso88@msudenver.edu>
2023-04-12 17:00:39 -0600

    properly ignoring popup.html

--- crawl.js
+++ crawl.js
@@ -1,40 +1,4 @@
-// const browser = window.browser || window.chrome;
-
-// browser.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-//   // Check if the page has finished loading
-//   if (changeInfo.status === "complete") {
-//     // Inject the payload.js script into the current tab
-//     browser.tabs.executeScript(tabId, { file: "payload.js" });
-
-//     // Listen to messages from the payload.js script and write to popout.html
-//   }
-// });
-
-// browser.runtime.onMessage.addListener(function (message) {
-//   let webPageString = message.html;
-//   const parser = new DOMParser();
-//   const htmlDocument = parser.parseFromString(webPageString, "text/html");
-//   const title = htmlDocument.title;
-
-//   let data = localStorage.getItem(title);
-
-//   if (data) {
-//     console.log(title + " has already been scraped");
-//     console.log(parser.parseFromString(data, "text/html"));
-//   } else {
-//     localStorage.setItem(title, webPageString);
-//     console.log("Website: " + title);
-//     console.log(htmlDocument);
-//   }
-
-//   if (htmlDocument.documentElement.innerHTML.includes("cart")) {
-//     console.log("Found the word 'cart' in the HTML document");
-//   } else {
-//     console.log("No cart found");
-//   }
-// });
-
-const browser = window.browser || window.chrome;
+// let browser = window.browser || window.chrome;
 
 browser.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   // Check if the page has finished loading
@@ -46,37 +10,38 @@ browser.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   }
 });
 
-browser.runtime.onMessage.addListener(function (message, sender) {
-  // check if the message was sent from the content script
-  if (sender.tab && sender.tab.id) {
-    let webPageString = message.html;
-    const parser = new DOMParser();
-    const htmlDocument = parser.parseFromString(webPageString, "text/html");
-    const title = htmlDocument.title;
+// browser.runtime.onMessage.addListener(function (message) {
+//   let webPageString = message.html;
+//   const parser = new DOMParser();
+//   const htmlDocument = parser.parseFromString(webPageString, "text/html");
+//   const title = htmlDocument.title;
 
-    let data = localStorage.getItem(title);
+//   console.log("Wesbite: " + title);
+//   console.log(htmlDocument);
 
-    if (data) {
-      console.log(title + " has already been scraped");
-      console.log(parser.parseFromString(data, "text/html"));
-    } else {
-      localStorage.setItem(title, webPageString);
-      console.log("Website: " + title);
-      console.log(htmlDocument);
-    }
+//   if (htmlDocument.documentElement.innerHTML.includes("cart")) {
+//     console.log("Found the word 'cart' in the HTML document");
+//   } else {
+//     console.log("No cart found");
+//   }
+// });
 
-    if (htmlDocument.documentElement.innerHTML.includes("cart")) {
-      console.log("Found the word 'cart' in the HTML document");
-    } else {
-      console.log("No cart found");
-    }
-  } else {
-    // set the "ignoreUpdates" flag in localStorage to ignore the message
-    localStorage.setItem("ignoreUpdates", true);
+browser.runtime.onMessage.addListener(function (message) {
+  if (!message.html) {
+    return;
   }
-});
 
-// add listener to remove the "ignoreUpdates" flag from localStorage
-window.addEventListener("unload", function () {
-  localStorage.removeItem("ignoreUpdates");
+  let webPageString = message.html;
+  const parser = new DOMParser();
+  const htmlDocument = parser.parseFromString(webPageString, "text/html");
+  const title = htmlDocument.title;
+
+  console.log("Website: " + title);
+  console.log(htmlDocument);
+
+  if (htmlDocument.documentElement.innerHTML.includes("cart")) {
+    console.log("Found the word 'cart' in the HTML document");
+  } else {
+    console.log("No cart found");
+  }
 });
--- payload.js
+++ payload.js
@@ -1,28 +1,6 @@
-const browser = window.browser || window.chrome;
-
-// send the page html as a chrome message
-browser.runtime.sendMessage({ html: document.documentElement.innerHTML });
-
-// const browser = window.browser || window.chrome;
-
-// function sendPageHtml() {
-//   // check if the "sentPageHtml" flag is set in localStorage
-//   if (!localStorage.getItem("sentPageHtml")) {
-//     // send the page html as a chrome message
-//     browser.runtime.sendMessage({ html: document.documentElement.innerHTML });
-
-//     // set the "sentPageHtml" flag in localStorage
-//     localStorage.setItem("sentPageHtml", true);
-//   }
-// }
-
-// // add event listener for the DOMContentLoaded event
-// document.addEventListener("DOMContentLoaded", sendPageHtml, { once: true });
-
-// // add message listener to ignore any updates
-// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-//   if (message.ignoreUpdates) {
-//     // remove the "sentPageHtml" flag from localStorage
-//     localStorage.removeItem("sentPageHtml");
-//   }
-// });
+browser.runtime.sendMessage(
+  { html: document.documentElement.innerHTML },
+  function (response) {
+    console.log("Response received:", response);
+  }
+);
